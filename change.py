ff759b5d94c59ebb98051b4726962e8970196eec
flordonlo <162724419+flordonlo@users.noreply.github.com>
2024-04-03 09:18:29 -0300

    Update change.py

--- change.py
+++ change.py
@@ -3,7 +3,7 @@ def change():
     money = 100
     print(f"Ingresar gasto: \n{expense}")
     print(f"Dinero recibido: \n{money}")
-    print(f"Vuelto \n")
+    print(f"\nVuelto \n")
     print(f"Pesos: \n{int(money-expense)}")
     print(f"Centavos: \n{int(((money-expense)-int(money-expense))*100)}")
 change()
